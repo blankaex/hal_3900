2dc9b41c8535e1c85ced55cb6904697bada2c46b
Ellen <ellenloates@gmail.com>
2019-03-09 22:45:12 +1100

    clean up scraper iteration 1: for single page, next step crawling

--- scraper/src/node/index.js
+++ scraper/src/node/index.js
@@ -1,30 +1,22 @@
-// this page is the entry point
-
 // require modules
 const fs = require('fs');
-
 const rp = require('request-promise');
-const otcsv = require('objects-to-csv');
 const cheerio = require('cheerio');
 
 const baseURL = 'https://webcms3.cse.unsw.edu.au/COMP1521/18s2/';
 const outlineURL = 'outline';
 
-const getPage = async () => {
-
+const getPageData = async () => {
     const html = await rp(baseURL + outlineURL);
+    // fs.writeFileSync("output.html", html);   -- use this to output HTML file and examine
 
     var $ = cheerio.load(html);
 
-    // BASIC GET INSTANCES OF TAG TO SINGLE TEXT
-    // const allTextRows = $("tr").text();
-    // const allParagraphs = $("p").text();
-
-    // get array of tables: each table is array of tablerows' text
+    // GET ALL TABLES DATA
     const allTables = [];
     $("table").map((index, element)=> {
         // TODO ideally we want the table's name/heading too.
-        const id = "table" + index;
+        const type = "table";
         let tableRows = [];
         $(element).find("tr").map((i,e)=> {
             // get the tablerow text, strip whitespace to singles
@@ -37,26 +29,20 @@ const getPage = async () => {
         allTables.push({type, tableRows});
     });
 
-    // test it by printing :)
-    let stream = fs.createWriteStream("outline.json", {flags:'a'});
-
-    allTables.forEach((t)=> {
-        stream.write(JSON.stringify(t) + "\n");
-        t.tableRows.forEach((r)=> {
-            console.log(r);
-        });
-        console.log("");
+    // GET ALL PARAGRAPHS DATA
+    const allParagraphs = [];
+    $("p").map((index, element)=> {
+        const type = "paragraph";
+        // paragraph with stripped whitespace. could break them up further if needed
+        const text = $(element).text().replace(/\s+/g,' ');
+        allParagraphs.push({type}, text);
     });
 
-    // each element is a table row: .text gets it to text
-    // $("tr").map((index, element)=> {
-    //     allTableRows.push($(element).text().replace(/\s+/g,' '));
-    // });
-
-    // fs.writeFileSync("outline.txt", allTextRows);
-    // fs.writeFileSync("outline.txt", allParagraphs);
-
-    return allTables;
+    // WRITE OUT TO JSON
+    const name = "course-outline";
+    return {name, allTables, allParagraphs};
 };
-getPage();
-// getPage().then(result => console.log(result.replace(/\s+/g,' ')));
+
+getPageData().then(result => {
+    fs.writeFileSync(result.name + ".json", JSON.stringify(result));
+});
