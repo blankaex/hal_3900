414c000f1855b6d2532201ba03489764c645fde9
Ellen <ellenloates@gmail.com>
2019-03-09 22:04:49 +1100

    now scrapes tables from course outline, creates array of js objects, writes to file

--- scraper/src/node/index.js
+++ scraper/src/node/index.js
@@ -14,15 +14,49 @@ const getPage = async () => {
 
     const html = await rp(baseURL + outlineURL);
 
-    fs.writeFileSync("output.html", html);
+    var $ = cheerio.load(html);
 
-    return html;
-   // var $ = cheerio.load(html);
+    // BASIC GET INSTANCES OF TAG TO SINGLE TEXT
+    // const allTextRows = $("tr").text();
+    // const allParagraphs = $("p").text();
 
-   // const allTextRows = $("tr").text();
-    // console.log(html);
-    //return Promise.all(pageMap);
-    // return allTextRows;
+    // get array of tables: each table is array of tablerows' text
+    const allTables = [];
+    $("table").map((index, element)=> {
+        // TODO ideally we want the table's name/heading too.
+        const id = "table" + index;
+        let tableRows = [];
+        $(element).find("tr").map((i,e)=> {
+            // get the tablerow text, strip whitespace to singles
+            const text = $(e).text().replace(/\s+/g,' ');
+            // tableRows.push($(e).text().replace(/\s+/g,' '));
+            // construct js object
+            tableRows.push({text});
+            // console.log(tableRows[i]);
+        });
+        allTables.push({type, tableRows});
+    });
+
+    // test it by printing :)
+    let stream = fs.createWriteStream("outline.json", {flags:'a'});
+
+    allTables.forEach((t)=> {
+        stream.write(JSON.stringify(t) + "\n");
+        t.tableRows.forEach((r)=> {
+            console.log(r);
+        });
+        console.log("");
+    });
+
+    // each element is a table row: .text gets it to text
+    // $("tr").map((index, element)=> {
+    //     allTableRows.push($(element).text().replace(/\s+/g,' '));
+    // });
+
+    // fs.writeFileSync("outline.txt", allTextRows);
+    // fs.writeFileSync("outline.txt", allParagraphs);
+
+    return allTables;
 };
-
-getPage().then(result => console.log(result));
+getPage();
+// getPage().then(result => console.log(result.replace(/\s+/g,' ')));
