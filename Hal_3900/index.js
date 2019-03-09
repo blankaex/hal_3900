8e0e14f769ed57fc6abf131ea9778031b01d3b4d
blankaex <nightmare_blankaex@hotmail.com>
2019-03-09 21:59:45 +1100

    node.js boilerplate first iteration

---
+++ Hal_3900/index.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+
+    // Build file path
+    let filePath = path.join(
+        __dirname, "public", req.url === "/" ? "index.html" : req.url
+    );
+
+    // Get file extension
+    let ext = path.extname(filePath);
+
+    // Initial content type
+    let contentType = "text/html";
+
+    // Check ext and set content type
+    switch (ext) {
+        case ".js":
+            contentType = "text/javascript"; break;
+        case ".css":
+            contentType = "text/css"; break;
+        case ".json":
+            contentType = "application/json"; break;
+        case ".png":
+            contentType = "image/png"; break;
+        case ".jpg":
+            contentType = "image/jpg"; break;
+    }
+
+    // Check if contentType is text/html but no .html file extension
+    if(contentType == "text/html" && ext == "") filePath += ".html";
+
+    // debug log
+    console.log(filePath);
+
+    // Read File
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            // Page not found
+            if(err.code == "ENOENT") {
+                fs.readFile(
+                    path.join(__dirname, "public", "404.html"),
+                    (err, content) => {
+                        res.writeHead(200, { "Content-Type": "text/html" });
+                        res.end(content, "utf8");
+                    }
+                );
+            } else {
+                //  Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(content, "utf8");
+        }
+    });
+});
+
+const PORT = process.env.PORT || 9447;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ Hal_3900/readme.md
@@ -0,0 +1,21 @@
+# Hal\_3900
+
+## Quick Start
+```bash
+# Ensure node.js v8.10.0 is installed
+$ node -v
+v8.10.0
+
+# Ensure npm v3.5.2 is installed
+$ npm -v
+3.5.2
+
+# Install dependencies
+$ npm install
+
+# Serve on localhost:9447
+$ npm start
+
+# Run with nodemon
+$ npm run dev
+```
