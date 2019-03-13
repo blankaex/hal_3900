9caebf6c7eb32662061896ed94cc6db3cd436503
Ellen <ellenloates@gmail.com>
2019-03-13 20:15:17 +1100

    progress on scraper fixed promise rejection bug

--- scraper/src/node/index.js
+++ scraper/src/node/index.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
-const baseURL = 'https://webcms3.cse.unsw.edu.au/COMP1521/18s2/';
-const outlineURL = 'outline';
+const baseURL = 'https://webcms3.cse.unsw.edu.au/';
+// const outlineURL = 'outline';
 
-const getPageData = async () => {
-    const html = await rp(baseURL + outlineURL);
+const getPageData = async (linkInfo) => {
+
+    const html = await rp(baseURL + linkInfo.linkURL);
     // fs.writeFileSync("output.html", html);   -- use this to output HTML file and examine
 
     var $ = cheerio.load(html);
@@ -39,10 +40,51 @@ const getPageData = async () => {
     });
 
     // WRITE OUT TO JSON
-    const name = "course-outline";
+    const name = linkInfo.title.replace(/\s+/g,'-');
     return {name, allTables, allParagraphs};
 };
 
-getPageData().then(result => {
-    fs.writeFileSync(result.name + ".json", JSON.stringify(result));
-});
+const scrapePages = async ()=> {
+    // setup: init queue for graph BFS "seen" set for web crawling
+    // MIGHT BE ABLE TO LIMIT DEPTH TO AVOID THE LITTLE LINKS WE DON'T WANT
+    // ONLY USE LINKS GOING TO OUR PAGES, AVOID OUTSIDE -> STARTING WITH "/"
+    let seenSet = new Set();
+    const addressList = []; // use a queue library for this to avoid it being inefficient??
+
+    const homePage = "COMP1521/18s2/";
+    const forumBase = "COMP1521/18s2/forums/";
+    // step 1: get first URLS to follow
+    let html;
+    try {
+        html = await rp(baseURL + homePage);
+    } catch (err){
+        console.log('Http Error');
+        return;
+    }
+    // fs.writeFileSync("home.html", html);
+    var $ = cheerio.load(html);
+    $("a").map((index, element)=> {
+        const link = $(element).attr("href");
+        if (link.toString().startsWith("/COMP1521/18s2/")){
+            addressList.push({
+                title: $(element).text(),
+                linkURL: $(element).attr("href").toString()
+            });
+        }
+    });
+    console.log(addressList);
+    // step 2: follow links in BFS pattern
+    for (let i=0; i<addressList.length; i++){
+            getPageData(addressList[i]).then(result =>
+                fs.writeFileSync("src///data///" + result.name + ".json", JSON.stringify(result)))
+                .catch( err =>
+                    console.log("couldn't read page " + addressList[i].linkURL));
+    }
+
+};
+
+scrapePages();
+
+// getPageData().then(result => {
+//     fs.writeFileSync(result.name + ".json", JSON.stringify(result));
+// });
