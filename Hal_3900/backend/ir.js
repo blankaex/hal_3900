5a753e241f606a21e127785c6e7dd17118e4c605
Zain Afzal <zain.afz@gmail.com>
2019-04-05 01:19:21 +1100

    init

--- Hal_3900/backend/ir.js
+++ Hal_3900/backend/ir.js
@@ -1,57 +1,25 @@
-//candidate: [{"tags":
-//              "text":},{}...]
-function sortNumber(a,b)
-{
-    return b - a
+// Given a candiate and a list of tags
+// calculates it's score
+function calcScore(tags, candidate) {
+    return candidate.tags.reduce(tag => tags.includes(tag["name"]) ? tag.salience : 0, 0);
 }
 // input: tags extracted by dialogflow
 //        candidates which have the tags, got by db_query.js
-const ir = function (tags, candidates) {
-    var scores = [];
-    var sc;
+function performIR(tags, candidates) {
     //calculate scores for each candidate
-    for(var i = 0; i< candidates.length;i++){
-        sc = 0;
-        for(var j = 0; j< tags.length;j++){
-            var sailence = 0;
-            //var theta = 0;
-            for(var k =0; k< candidates[i].tags.length;k++){
-                if (candidates[i].tags[k]["name"] == tags[j]){
-                    sailence = candidates[i].tags[k].salience;
-                    //theta = candidates[i].tags[k].theta;
-                    sc += sailence //* theta;
-                }
-            }
-
+    candidates = candidates.map((candidate,i)=>{
+        return {
+            ...candidate,
+            _score: calcScore(tags, candidate)
         }
-        scores.push(sc);
-    }
-    var sortScores = [...scores];
-    sortScores.sort(sortNumber); // sort scores
-    //console.log(sortScores);
-    var response=[];
-    var responseIndex = [];
-    responseIndex.push(candidates[scores.indexOf(sortScores[0])]);
-    //avoid redundency
-    if(responseIndex[0]!=candidates[scores.indexOf(sortScores[1])]){
-        responseIndex.push(candidates[scores.indexOf(sortScores[1])])
-    }
-    if(candidates[scores.indexOf(sortScores[1])]!=candidates[scores.indexOf(sortScores[2])]){
-        responseIndex.push(candidates[scores.indexOf(sortScores[2])])
-    }
-    response.push(responseIndex[0]);
-    response.push(responseIndex[1]);
-    response.push(responseIndex[2]);
-    /* re-arrange to unify the format of entities from block and forum
-    for(var y = 0; y<response.length;y++){
-        if(!response[y].hasOwnProperty("text")){
-
-
-        }
-    }
-     */
-    return response; //output response array, each entity is in its original format
+    });
+    // sort by score
+    candidates = candidates.sort((a,b)=> b._score - a._score);
+    
+    // filter out duplicates
+    const response= candidates.filter((value, index, self)=>self.indexOf(value) === index);
+    
+    return response.slice(0,4); // output top 3 results
 };
 
-
-module.exports = {sortNumber,ir};
+module.exports = {performIR};
