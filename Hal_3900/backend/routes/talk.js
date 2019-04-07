8e89d8feaffdfe81790cf84180f81a6011192940
blankaex <nightmare_blankaex@hotmail.com>
2019-04-07 17:21:49 +1000

    moved talk to a route

---
+++ Hal_3900/backend/routes/talk.js
@@ -0,0 +1,28 @@
+const bot = require('../bot');
+const hal = new bot()
+
+function sendHalResponse(msg) {
+    hal.query(msg.text)
+        .then(r=>JSON.stringify({
+            type: 'message',
+            error: false,
+            data: r
+        }))
+}
+
+async function respond(ws, msg) {
+    msg = JSON.parse(msg)
+    if (msg.type === 'message') {
+        ws.send(await sendHalResponse(msg))
+    } else {
+      ws.send(JSON.stringify({
+        type: 'error',
+        error: true,
+        msg: 'Unknown Message Type'
+      }))
+    }
+}
+
+module.exports = function talkSocket(ws) {
+    ws.on('message', msg => respond(ws, msg))
+}
