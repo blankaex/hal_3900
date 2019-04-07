7fb770146145317b5bb10b7e1e18227aacea6283
blankaex <nightmare_blankaex@hotmail.com>
2019-04-07 17:10:35 +1000

    implemented with mongodb

--- Hal_3900/backend/routes/api/users.js
+++ Hal_3900/backend/routes/api/users.js
@@ -1,39 +1,19 @@
 const express = require('express');
-const session = require('express-session');
-const mongodb = require('mongodb');
-const mongoose = require('mongoose');
-const dbUrl = 'mongodb://localhost:27017';
-const dbName = 'database'
-
 const router = express.Router();
-
-const UserSchema = new mongoose.Schema({
-    id: {
-          type: Number,
-          unique: true,
-          required: true,
-          trim: true
-        },
-    name: {
-          type: String,
-          unique: true,
-          required: true,
-          trim: true
-        }
-});
-const User = mongoose.model('User', UserSchema);
-
-// test "db"
-const db = [
-	{ id: 0, name: 'Ellen' },
-	{ id: 1, name: 'Hayden' },
-	{ id: 2, name: 'Yi' },
-	{ id: 3, name: 'Zain' }
-];
+const DB = require('../../db');
+const db = new DB();
 
 // get all users
 router.get('/', async (req, res) => {
-	res.status(200).json(db);
+    if (!db.connected)
+        await db.connect();
+
+    const result = await db.search({}, 'users');
+
+	if (result.length > 0)
+		res.status(200).send(result);
+    else
+        res.status(400).send("no users");
 });
 
 // get current user
@@ -41,19 +21,25 @@ router.get('/me', async (req, res) => {
 	if(req.session.user)
 		res.status(200).json(req.session.user);
 	else
-		res.status(200).send('u r no one');
+		res.status(200).send('please log in');
 });
 
 // sets the user of the current session
-router.get('/:name', (req, res) => {
-    req.session.user = req.params.name; // should technically be done at the end
-										// but what could go wrong
-	if (db.some(db => db.name === req.params.name)) {
-		res.status(200).json(db.filter(db => db.name === req.params.name));
-	} else {
-		db.push({id: db.length, name: req.params.name});
-		res.status(200).json({ msg: `Created user ${req.params.name}` });
-	}
+router.post('/set', async (req, res) => {
+    if(req.session.user != req.body.zid) {
+        if (!db.connected)
+            await db.connect();
+
+        const query = { zid: { $eq: req.body.zid } };
+        const result = await db.search(query, 'users');
+
+        if (result.length == 0)
+            db.addToCollection([ { zid: req.body.zid } ], 'users');
+
+        req.session.user = req.body.zid;
+    }
+
+    res.status(200).send(req.session.user);
 });
 
 module.exports = router;
