f0227cdad72fd505ebac9b4d2f373bcacbf08eb3
Ellen <ellenloates@gmail.com>
2019-04-08 22:14:51 +1000

    login page - needs to connect to backend

--- Hal_3900/frontend/src/views/Login.vue
+++ Hal_3900/frontend/src/views/Login.vue
@@ -7,32 +7,33 @@
   </div>
 </template>
 
-<!--TODO make it actually log in :'( -->
-
 <script lang="ts">
 import { Vue } from 'vue-property-decorator'
 
 // export default class Login extends Vue
-export default {
-  name: 'Login',
-  data() {
-    return {
-      username: '',
-      password: ''
-    }
-  },
-  methods: {
-    login(){
+export default class Login extends Vue {
+  username: any;
+  password: any;
+  login () {
+    if (this.username && this.password) {
       console.log(this.username)
-      if (this.username && this.password){
-        // needs to send credentials to backend, and receive cookie back
-
-      }
+      console.log('awkward plaintext password')
+      // TODO needs to send credentials to backend, and receive cookie back
     }
   }
 }
 </script>
 
 <style scoped lang="sass">
+.login
+  width: 100%
+  height: 100%
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  margin-top: auto
+  color: #FFFFFF
+  background-color: #5C5C5C
 
 </style>
