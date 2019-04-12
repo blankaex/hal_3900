034574a88387bb626104cf3670ff7e60aac187aa
Ellen <ellenloates@gmail.com>
2019-04-12 21:48:12 +1000

    api post working, redirect todo

--- Hal_3900/frontend/src/views/Login.vue
+++ Hal_3900/frontend/src/views/Login.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="login">
-    <h3>Login:</h3>
-    <input type="text" name="username" v-model.trim="username" placeholder="Username"/>
-    <input type="password" name="password" v-model.trim="password" placeholder="Password"/>
-    <button type="button" v-on:click="login()">Login</button>
+    <form>
+      <h3>Login:</h3>
+      <input type="text" name="username" v-model.trim="username" placeholder="zID"/>
+      <!--    <input type="password" name="password" v-model.trim="password" placeholder="Password"/>-->
+      <button type="button" v-on:click.prevent="login()">SIGN IN</button>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue } from 'vue-property-decorator'
+import router from "@/router";
 
 // export default class Login extends Vue
 export default class Login extends Vue {
   username: any;
-  password: any;
   login () {
-    if (this.username && this.password) {
-      console.log(this.username)
-      console.log('awkward plaintext password')
-      // TODO needs to send credentials to backend, and receive cookie back
+    if (this.username) {
+
+      let host = 'backend.hal-3900.com'
+      if (window.location.host !== 'hal-3900.com') {
+        host = 'localhost:9447'
+      }
+      this.$http.post(`http://${host}/api/users/set`, { body: { zid: this.username } })
+        .then(data => console.log(data))
+      this.$router.push('/')
     }
   }
 }
