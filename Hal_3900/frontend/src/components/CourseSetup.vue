dd776fb3fbc55ace9f7712b7ed5298d8739da169
Ellen <ellenloates@gmail.com>
2019-04-13 10:28:59 +1000

    started admin setup page

---
+++ Hal_3900/frontend/src/components/CourseSetup.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="setup">
+      <form>
+        <input type="text" v-model.trim="courseCode" placeholder="Course Code"/>
+        <input type="text" v-model.trim="forum" placeholder="Forum URL"/>
+        <input type="text" v-model.trim="outline.1" placeholder="Course Outline"/>
+        <input v-for="item in assignment" v-model.trim="item.name" @change.once="addAssignment()"/>
+        <input v-for="item in content" v-model.trim="item.name" @change.once="addAssignment()"/>
+        <button type="submit" v-on:click.prevent="send()">SUBMIT</button>
+     </form>
+    </div>
+</template>
+
+<script lang="ts">
+  import { Vue } from 'vue-property-decorator'
+
+  export default class CourseSetup extends Vue {
+    courseCode: string =''
+    forum: string=''
+    outline: [{
+      name: '',
+      url: ''
+    }]
+    assignment: []
+    content: []
+    send() {
+      let host = 'backend.hal-3900.com'
+      if (window.location.host !== 'hal-3900.com') {
+        host = 'localhost:9447'
+      }
+      //TODO make sure wrapped as correct formatted JS object
+      //TODO post to backend API (not set up yet)
+      //TODO then load confirmation page w/option to go back to admin
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- Hal_3900/frontend/src/views/Login.vue
+++ Hal_3900/frontend/src/views/Login.vue
@@ -4,7 +4,7 @@
       <h3>Login:</h3>
       <input type="text" name="username" v-model.trim="username" placeholder="zID"/>
       <!--    <input type="password" name="password" v-model.trim="password" placeholder="Password"/>-->
-      <button type="button" v-on:click.prevent="login()">SIGN IN</button>
+      <button type="submit" v-on:click.prevent="login()">SIGN IN</button>
     </form>
 <!--    <div v-if="submitted">login failed</div>-->
   </div>
@@ -16,17 +16,19 @@ import { Vue } from 'vue-property-decorator'
 // export default class Login extends Vue
 export default class Login extends Vue {
   username: any;
-  // submitted: false;
   login () {
     if (this.username) {
+      // get URL for backend
       let host = 'backend.hal-3900.com'
       if (window.location.host !== 'hal-3900.com') {
         host = 'localhost:9447'
       }
+      // post login credentials to backend
       this.$http.post(`http://${host}/api/users/set`, { body: { zid: this.username } })
         .then(res => {
           console.log(res)
           if (res.ok) {
+            // if backend response ok, set authed in store
             this.$store.commit('login', this.username)
             this.$router.push({ name: 'home' })
           }
