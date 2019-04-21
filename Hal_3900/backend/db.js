6890bb3cb8ba62af40d5fbcad7f9c9a1bc021941
Zain Afzal <zain.afz@gmail.com>
2019-04-21 22:30:58 +1000

    removed logging

--- Hal_3900/backend/db.js
+++ Hal_3900/backend/db.js
@@ -193,9 +193,6 @@ module.exports = class DB {
 		candidates = candidates.concat(await this.findAllFromCollection('forum'));
 
 		// kill all non intent matches items 
-		logger.error(candidates);
-		logger.error(candidates.map(x=>x.intent));
-		logger.error(intent);
 		candidates = candidates.filter(c=>c.intent === intent)
 
 		// calculate scores for each candidate
